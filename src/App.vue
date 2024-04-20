b06394887e2bf40bf9de7fdc627b0aa8a54f8bc9
dexstrong <ilya.yurev.04@inbox.ru>
2024-04-20 20:04:06 +0300

    cart layout

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,38 @@
 <script setup>
-  import CardList from './components/CardList.vue';
-  import Header from './components/Header.vue';
+import Header from './components/Header.vue'
+import CardList from './components/CardList.vue'
+import Drawer from './components/Drawer.vue'
 </script>
 
 <template>
+  <!-- <Drawer /> -->
+
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-10">
     <Header />
-    
-    <div class="p-10"> 
-      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+
+    <div class="p-10">
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+
+        <div class="flex gap-4">
+          <select class="py-3 px-4 border rounded-md outline-none">
+            <option>По названию</option>
+            <option>По цене (дешевые)</option>
+            <option>По цене (дорогие)</option>
+          </select>
+
+          <div class="relative">
+            <img class="absolute left-4 top-3" src="/search.svg" />
+            <input
+              class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+              type="text"
+              placeholder="Поиск..."
+            />
+          </div>
+        </div>
+      </div>
+
       <CardList />
-      
     </div>
   </div>
 </template>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,14 +1,20 @@
 <script setup>
-    import Card from './Card.vue'
+import Card from './Card.vue'
+
+const onClickAdd = () => {
+  alert('Добавлено!')
+}
 </script>
 
 <template>
   <div class="grid grid-cols-4 gap-5">
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    <Card
+      title="Мужские кроссовки Nike Blazer Mid Suede"
+      image-url="/sneakers/sneakers-1.jpg"
+      :price="5000"
+      :is-added="false"
+      :is-favorite="false"
+      :on-click-add="onClickAdd"
+    />
   </div>
 </template>
